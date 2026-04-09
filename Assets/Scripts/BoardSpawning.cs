658a66c35c6dcebd1778bf36facf1b5976e474ef
Marius0101 <mariusoctavian010@gmail.com>
2026-04-09 22:16:05 +0300

    generate board

---
+++ Assets/Scripts/BoardSpawning.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BoardSpawning : MonoBehaviour
+{
+    public GameObject squareTemplate;
+    public int numberOfSquares = 8;
+    void Start()
+    {
+        for (int i = 0; i < numberOfSquares; i++)
+        {
+            for(int j = 0; j < numberOfSquares; j++)
+            {
+                Vector3 position = new Vector3(i, j, 0);
+                
+                GameObject square = Instantiate(squareTemplate, position, Quaternion.identity);
+                Renderer renderer = square.GetComponent<Renderer>();
+                if ((i + j) % 2 == 0)
+                {
+                    renderer.material.color = Color.white;
+                }
+                else
+                {
+                    renderer.material.color = Color.black;
+                }
+            }
+        }
+    }
+}
