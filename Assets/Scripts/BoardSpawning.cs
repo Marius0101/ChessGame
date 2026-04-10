bb7918d3ec1e30e44b7c6e79d2d5b681687c51a5
Marius0101 <mariusoctavian010@gmail.com>
2026-04-10 21:29:42 +0300

    add pieces first version

--- Assets/Scripts/BoardSpawning.cs
+++ Assets/Scripts/BoardSpawning.cs
@@ -1,11 +1,18 @@
 ﻿using Assets.DataModels;
 using System;
 using UnityEngine;
+using static Unity.VisualScripting.Dependencies.Sqlite.SQLite3;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class BoardSpawning : MonoBehaviour
 {
     public GameObject squarePrefab;
     public GameObject pawnPrefab;
+    public GameObject bishopPrefab;
+    public GameObject knightPrefab;
+    public GameObject rookPrefab;
+    public GameObject queenPrefab;
+    public GameObject kingPrefab;
     public int numberOfSquares = 8;
     private SquareData[,] board;
     private GameObject[,] visualSquares;
@@ -17,6 +24,7 @@ public class BoardSpawning : MonoBehaviour
         {
             for(int j = 0; j < numberOfSquares; j++)
             {
+                board[i, j] = new SquareData();
                 Vector3 position = new Vector3(i, j, 0);
                 
                 GameObject square = Instantiate(squarePrefab, position, Quaternion.identity);
@@ -33,27 +41,75 @@ public class BoardSpawning : MonoBehaviour
 
     private void SpawnTestPieces()
     {
-        board[0, 1].Piece = new Piece(PieceType.Pawn, ColorType.White);
-        UpdateVisualAt(0, 1);
+        board[0, 0].Piece = new Piece(PieceType.Rook, ColorType.White);
+        board[1, 0].Piece = new Piece(PieceType.Pawn, ColorType.White);
+        board[0, 1].Piece = new Piece(PieceType.Knight, ColorType.White);
+        board[0, 3].Piece = new Piece(PieceType.Queen, ColorType.White);
+        board[0, 2].Piece = new Piece(PieceType.Bishop, ColorType.White);
+        board[0, 4].Piece = new Piece(PieceType.King, ColorType.White);
+
+        board[7, 0].Piece = new Piece(PieceType.Rook, ColorType.Black);
+        board[6, 0].Piece = new Piece(PieceType.Pawn, ColorType.Black);
+        board[7, 1].Piece = new Piece(PieceType.Knight, ColorType.Black);
+        board[7, 3].Piece = new Piece(PieceType.Queen, ColorType.Black);
+        board[7, 2].Piece = new Piece(PieceType.Bishop, ColorType.Black);
+        board[7, 4].Piece = new Piece(PieceType.King, ColorType.Black);
+
+        for(int i =0 ; i < numberOfSquares; i++)
+        {
+            UpdateVisualAt(0, i);
+            UpdateVisualAt(1, i);
+            UpdateVisualAt(6, i);
+            UpdateVisualAt(7, i);
+        }
+
     }
 
     private void UpdateVisualAt(int row, int column)
     {
-        PieceType piece = board[row, column].Piece.Type;
-        if (piece == PieceType.None)
+        Piece piece = board[row, column].Piece;
+        if (piece == null || piece.Type == PieceType.None)
             return;
         GameObject prefabToSpawn = null;
 
-        switch (piece)
+        switch (piece.Type)
         {
             case PieceType.Pawn:
                 prefabToSpawn = pawnPrefab;
+                break;
+            case PieceType.Bishop:
+                prefabToSpawn = bishopPrefab;
+                break;
+            case PieceType.Knight:
+                prefabToSpawn = knightPrefab;
+                break;
+            case PieceType.Rook:
+                prefabToSpawn = rookPrefab;
+                break;
+            case PieceType.Queen:
+                prefabToSpawn = queenPrefab;
+                break;
+            case PieceType.King:
+                prefabToSpawn = kingPrefab;
                 break;
         }
         if (prefabToSpawn != null)
         {
             Vector3 position = new Vector3(column, row, -2);
-            Instantiate(prefabToSpawn, position, Quaternion.identity);
+            GameObject  newPiece= Instantiate(prefabToSpawn, position, Quaternion.identity);
+            ApplyColor(newPiece, piece.Color);
+        }
+    }
+
+    private void ApplyColor(GameObject pieceObject, ColorType color)
+    {
+        SpriteRenderer outline = pieceObject.transform.Find("Outline").GetComponent<SpriteRenderer>();
+        SpriteRenderer Inline = pieceObject.transform.Find("Inline").GetComponent<SpriteRenderer>();
+
+        if (color == ColorType.Black)
+        {
+            outline.color = Color.white;
+            Inline.color = Color.black;
         }
     }
 
